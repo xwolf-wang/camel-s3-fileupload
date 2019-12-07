4d34496c8fc6b706a8214074dd211939251480d1
ming <wang_ming@iclould.com>
2019-12-07 22:22:41 +0800

    minor changes

--- src/main/java/com/javakia/api/camel/s3/fileupload/AWSS3Route.java
+++ src/main/java/com/javakia/api/camel/s3/fileupload/AWSS3Route.java
@@ -1,8 +1,11 @@
 package com.javakia.api.camel.s3.fileupload;
 
+import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.aws.s3.S3Constants;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 
 /**
  * A simple Camel route that moves an object from an endpoint to AWS S3 Bucket
@@ -18,6 +21,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class AWSS3Route extends RouteBuilder {
 
+	@Autowired
+	CamelContext camelContext;
+
 	@Override
 	public void configure() throws Exception {
 		from("{{from.component}}:{{from.endpoint}}").routeId("camelS3Route")
@@ -25,7 +31,7 @@ public class AWSS3Route extends RouteBuilder {
 				//This is the filename on the S3 bucket
 				.setHeader(S3Constants.KEY, simple("${in.header.CamelFileNameOnly}"))
 				//aws-s3 the camel component and  and that is how you tell camel where you want to upload. 
-				.to("aws-s3://{{awsS3BucketName}}?deleteAfterWrite=false&region={{awsRegion}}&accessKey={{awsAccessKey}}&secretKey=RAW({{awsAccessSecretKey}})")
+				.to("aws-s3://{{awsS3BucketName}}?deleteAfterWrite=false&deleteAfterRead=false&region={{awsRegion}}&accessKey={{awsAccessKey}}&secretKey=RAW({{awsAccessSecretKey}})")
 				//optionally logging success message
 				.log("${in.header.CamelFileNameOnly} succesfully uploaded to S3 {{awsS3BucketName}} bucket");
 	}
